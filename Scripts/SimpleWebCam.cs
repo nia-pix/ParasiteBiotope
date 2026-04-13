a905b8b7c62f8c65139a8467f8c65c68636e063b
nia-pix <printemps047@gmail.com>
2026-04-13 18:30:47 +0900

    Update SimpleWebCam.cs

--- Scripts/SimpleWebCam.cs
+++ Scripts/SimpleWebCam.cs
@@ -3,24 +3,24 @@ using UnityEngine.UI;
 
 public class SimpleWebCam : MonoBehaviour
 {
-    public RawImage rawImage; // 映像を映す場所
-    private WebCamTexture webCamTexture; // カメラのデータ
+    public RawImage rawImage;
+    private WebCamTexture webCamTexture; 
 
     void Start()
     {
-        // PCに繋がってるカメラを探す
+        // PCに繋がってるカメラ
         WebCamDevice[] devices = WebCamTexture.devices;
 
         if (devices.Length > 0)
         {
-            // 最初のカメラを使う（ここの数字を変えるとカメラが切り替わるよ）
+            
             webCamTexture = new WebCamTexture(devices[0].name, 1920, 1080, 30);
             
-            // RawImageに貼り付ける
+           
             rawImage.texture = webCamTexture;
             rawImage.material.mainTexture = webCamTexture;
 
-            // 録画（再生）スタート！
+           
             webCamTexture.Play();
         }
         else
